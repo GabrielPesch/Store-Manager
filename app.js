8276c6a759bdaa56dc05e860feb2db10782c7ead
Gabriel Pesch <gkpesch@gmail.com>
2022-06-29 14:36:24 -0300

    Iniciando o projeto store manager

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-// não remova esse endpoint, é para o avaliador funcionar
+// não remova esse endpoint, é para o avaliador funcionar!
 app.get('/', (_request, response) => {
   response.send();
 });
