2522f9a2980910fd57563a4b93a063da6ab9d32b
Gabriel Pesch <gkpesch@gmail.com>
2022-07-01 03:12:59 +0000

    chore(CONNECTION): connection agora usa dotenv
    A pool de conexão passou a utilizar o dotenv e esta da seguinte forma:
    
    const db = mysql.createPool({
      host: process.env.MYSQL_HOST,
      user: process.env.MYSQL_USER,
      password: process.env.MYSQL_PASSWORD,
      database: process.env.MYSQL_DATABASE || 'StoreManager',
    });

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const db = mysql.createPool({
-  host: 'db',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = db;
