0b231832c0d99ef523d05796513fa0919d912c50
Gabriel Pesch <gkpesch@gmail.com>
2022-07-07 00:28:58 -0300

    feat(Swagger): Criada a documentação pelo swagger

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+
 const errorHandler = require('./middlewares/errorHandler');
 require('express-async-errors');
 const productsRoute = require('./Routes/products');
 const salesRoute = require('./Routes/sales');
+const swaggerDocs = require('./swagger.json');
 
 const app = express();
+
 app.use(express.json());
 
 app.use('/products', productsRoute);
 app.use('/sales', salesRoute);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(errorHandler);
 
-// não remova esse endpoint, é para o avaliador funcionar!
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
